58a0a7ff85ef8dfa5b41d5fc28f3a6b73bd03749
nishitagiduturi <nishitaoct26@gmail.com>
2021-08-20 17:21:18 +0530

    Moved tasks state to parent component - App.js instead of using the state inside child component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Food shopping',
+      day: 'Monday',
+      remainder: false,
+    },
+    {
+      id: 2,
+      text: 'Laptop screen repair',
+      day: 'Tuesday',
+      remainder: false,
+    },
+    {
+      id: 3,
+      text: 'Cook',
+      day: 'Wednesday',
+      remainder: false,
+    },
+    {
+      id: 4,
+      text: 'Visit Dentist',
+      day: 'Thursday',
+      remainder: false,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header title="Task Tracker App" />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,4 @@
-import { useState } from 'react';
-
-const Tasks = () => {
-  // Now tasks is part of our state
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Food shopping',
-      day: 'Monday',
-      remainder: false,
-    },
-    {
-      id: 2,
-      text: 'Laptop screen repair',
-      day: 'Tuesday',
-      remainder: false,
-    },
-    {
-      id: 3,
-      text: 'Cook',
-      day: 'Wednesday',
-      remainder: false,
-    },
-    {
-      id: 4,
-      text: 'Visit Dentist',
-      day: 'Thursday',
-      remainder: false,
-    },
-  ]);
+const Tasks = ({ tasks }) => {
   return (
     <div className="tasks">
       {tasks.map((task) => {
